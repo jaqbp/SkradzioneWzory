f514334375dd4b137557b4ba7999b689228ebda4
H4wk <quasand89@gmail.com>
2023-11-18 22:59:12 +0100

    fastapi config

---
+++ backend/main.py
@@ -0,0 +1,33 @@
+from typing import Union
+
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.post("/api/check-similarity")
+async def check_similarity(request: Request):
+    body = await request.json()
+    text1 = body["text1"]
+    text2 = body["text2"]
+    # TODO: check similarity between text1 and text2
+    return {"similarity": 0.0}
