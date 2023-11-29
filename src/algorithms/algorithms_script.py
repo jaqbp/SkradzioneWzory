f634d39d772b335e1002251b609dda1a4523f9fe
Bartosz Smolarz <bartsmo430@student.polsl.pl>
2023-11-29 15:57:35 +0100

    changed files to have classes, tried to implement them

---
+++ src/algorithms/algorithms_script.py
@@ -0,0 +1,78 @@
+import os
+from ply import lex
+from jt_algorithm import JT_LatexTokenizer, JT_LatexSimilarityAnalyser
+from cosine_similarity import CSTextProcessor, CosineSimilarity
+from levenshtein import L_LatexAnalyser, L_FormulaComparer
+
+if __name__ == "__main__":
+
+    # Cosine Similarity
+
+    noise_words = {
+        "o", "w", "a", "i", "lub", "na", "z", "do", "jest", "nie", "się", "jak",
+        "tak", "że", "co", "te", "po", "za", "ale", "to", "od", "tym", "oraz",
+        "ani", "lecz", "więc", "aby"
+    }
+
+    text_processor = CSTextProcessor(noise_words)
+
+    text1 = "Przykładowy tekst numer jeden."
+    text2 = "Tekst przykładowy numer dwa."
+
+    words1 = text_processor.get_word_counts(text1)
+    words2 = text_processor.get_word_counts(text2)
+
+    similarity = CosineSimilarity.calculate_cosine_similarity(words1, words2)
+    print(f"Podobieństwo kosinusowe: {similarity}")
+
+    # Jaccard-Tanimoto
+
+    # Nie wiem na czym polega mój problem z tym błędem który dostaję, potrzebuję z tym trochę pomocy (przy przerabianiu Levenshteina też to jest)
+
+    jt_latex_similarity_analyser = JT_LatexSimilarityAnalyser()
+    jt_latex_tokenizer = JT_LatexTokenizer()
+
+    file_path1 = os.path.join(os.path.dirname(__file__), "tex_files/example2.tex")
+    with open(file_path1, "r") as file:
+        latex_content1 = "".join(file.readlines())
+    tokens1 = jt_latex_tokenizer.tokenize_latex(latex_content1)
+
+    file_path2 = os.path.join(os.path.dirname(__file__), "tex_files/document2.tex")
+    with open(file_path2, "r") as file:
+        latex_content2 = "".join(file.readlines())
+    tokens2 = jt_latex_tokenizer.tokenize_latex(latex_content2)
+
+    similarity_values = []
+    for token_set1 in tokens1:
+        for token_set2 in tokens2:
+            similarity = JT_LatexSimilarityAnalyser.calculate_similarity(token_set1, token_set2)
+            similarity_values.append(similarity)
+
+    average_similarity = sum(similarity_values) / len(similarity_values)
+    JT_LatexSimilarityAnalyser.generate_report(average_similarity)
+
+    # Levenshtein
+
+    latex_analyzer = L_LatexAnalyser()
+
+    file_path1 = os.path.join(os.path.dirname(__file__), "tex_files/example2.tex")
+    with open(file_path1, "r") as file:
+        latex_content1 = file.read()
+    math1 = latex_analyzer.extract_math(latex_content1)
+
+    file_path2 = os.path.join(os.path.dirname(__file__), "tex_files/document2.tex")
+    with open(file_path2, "r") as file:
+        latex_content2 = file.read()
+    math2 = latex_analyzer.extract_math(latex_content2)
+
+    formula_comparer = L_FormulaComparer()
+
+    print("Formulas from the first document:", math1)
+    print("Formulas from the second document:", math2)
+
+    for i, formula1 in enumerate(math1):
+        for j, formula2 in enumerate(math2):
+            similarity = L_FormulaComparer.compare_formulas(formula1, formula2)
+            print(
+                f"Similarity percentage between formula {i+1} from document 1 and formula {j+1} from document 2: {similarity:.2f}%"
+            )
--- src/algorithms/levenshtein.py
+++ src/algorithms/levenshtein.py
@@ -2,119 +2,113 @@ from ply import lex
 import os
 import re
 
-
-# File paths for LaTeX files
-file_path1 = "tex_files/example2.tex"
-file_path2 = "tex_files/document2.tex"
-
-# Token definitions for mathematical formulas and LaTeX commands
-tokens = (
-    "MATH_MODE_INLINE",
-    "MATH_MODE_DISPLAY",
-    "EQUATION_ENVIRONMENT",
-    "COMMAND",
-    "OTHER",
-)
-
-# Regex definitions for tokens
-t_MATH_MODE_INLINE = r"\$[^\$]*\$"
-t_MATH_MODE_DISPLAY = r"\\\[[^\]]*\\\]"
-t_EQUATION_ENVIRONMENT = r"\\begin\{equation\}.*?\\end\{equation\}"
-t_COMMAND = r"\\[a-zA-Z]+"
-t_ignore_OTHER = r"."  # Ignore other characters
-
-
-def t_error(t):
-    t.lexer.skip(1)
-
-
-lexer = lex.lex()
-
-
-def extract_math(latex_content):
-    lexer.input(latex_content)
-    math_expressions = []
-    while True:
-        tok = lexer.token()
-        if not tok:
-            break
-        if tok.type in [
+class L_LatexAnalyser:
+    def __init__(self):
+        # Token definitions for mathematical formulas and LaTeX commands
+        self.tokens = (
             "MATH_MODE_INLINE",
             "MATH_MODE_DISPLAY",
             "EQUATION_ENVIRONMENT",
-        ]:
-            # Remove LaTeX markers from the beginning and end of the expression
-            cleaned_expression = tok.value
-            if cleaned_expression.startswith("\\["):
-                cleaned_expression = cleaned_expression[2:]
-            if cleaned_expression.endswith("\\]"):
-                cleaned_expression = cleaned_expression[:-2]
-            math_expressions.append(cleaned_expression.strip())
-    return math_expressions
+            "COMMAND",
+            "OTHER",
+        )
 
+        self.t_MATH_MODE_INLINE = r"\$[^\$]*\$"
+        self.t_MATH_MODE_DISPLAY = r"\\\[[^\]]*\\\]"
+        self.t_EQUATION_ENVIRONMENT = r"\\begin\{equation\}.*?\\end\{equation\}"
+        self.t_COMMAND = r"\\[a-zA-Z]+"
+        self.t_ignore_OTHER = r"."  # Ignore other characters
+
+        self.lexer = lex.lex()
+
+    def t_error(t):
+        t.lexer.skip(1)
+
+    def extract_math(self, latex_content):
+        self.lexer.input(latex_content)
+        math_expressions = []
+        while True:
+            tok = self.lexer.token()
+            if not tok:
+                break
+            if tok.type in [
+                "MATH_MODE_INLINE",
+                "MATH_MODE_DISPLAY",
+                "EQUATION_ENVIRONMENT",
+            ]:
+                cleaned_expression = tok.value
+                if cleaned_expression.startswith("\\["):
+                    cleaned_expression = cleaned_expression[2:]
+                if cleaned_expression.endswith("\\]"):
+                    cleaned_expression = cleaned_expression[:-2]
+                math_expressions.append(cleaned_expression.strip())
+        return math_expressions
 
 # Extracting mathematical formulas
-with open(file_path1, "r") as file:
-    latex_content1 = file.read()
-math1 = extract_math(latex_content1)
+# file_path1 = os.path.join(os.path.dirname(__file__), "tex_files/example2.tex")
+# with open(file_path1, "r") as file:
+#     latex_content1 = file.read()
+# math1 = extract_math(latex_content1)
 
-with open(file_path2, "r") as file:
-    latex_content2 = file.read()
-math2 = extract_math(latex_content2)
+# file_path2 = os.path.join(os.path.dirname(__file__), "tex_files/document2.tex")
+# with open(file_path2, "r") as file:
+#     latex_content2 = file.read()
+# math2 = extract_math(latex_content2)
+
+class L_FormulaComparer:
+    @staticmethod
+    def levenshtein(a, b):
+        if not a:
+            return len(b)
+        if not b:
+            return len(a)
+
+        # Initialize a matrix of dimensions (len(a)+1) x (len(b)+1)
+        matrix = [[0] * (len(b) + 1) for _ in range(len(a) + 1)]
+
+        # Initialize the first column and first row
+        for i in range(len(a) + 1):
+            matrix[i][0] = i
+        for j in range(len(b) + 1):
+            matrix[0][j] = j
+
+        # Iterate through the matrix and update values
+        for i in range(1, len(a) + 1):
+            for j in range(1, len(b) + 1):
+                if a[i - 1] == b[j - 1]:
+                    cost = 0
+                else:
+                    cost = 1
+
+                matrix[i][j] = min(
+                    matrix[i - 1][j] + 1,
+                    matrix[i][j - 1] + 1,
+                    matrix[i - 1][j - 1] + cost,
+                )
+
+        return matrix[-1][-1]
+
+    @staticmethod
+    def normalize_formula(formula):
+        return formula.replace(" ", "")
+
+    @staticmethod
+    def compare_formulas(self, formula1, formula2):
+        formula1_norm = self.normalize_formula(formula1)
+        formula2_norm = self.normalize_formula(formula2)
+        distance = self.levenshtein(formula1_norm, formula2_norm)
+        max_length = max(len(formula1_norm), len(formula2_norm))
+        if max_length == 0:
+            return 100.0
+        return (1 - distance / max_length) * 100
 
 
-def levenshtein(a, b):
-    if not a:
-        return len(b)
-    if not b:
-        return len(a)
+    # print("Formulas from the first document:", math1)
+    # print("Formulas from the second document:", math2)
 
-    # Initialize a matrix of dimensions (len(a)+1) x (len(b)+1)
-    matrix = [[0] * (len(b) + 1) for _ in range(len(a) + 1)]
-
-    # Initialize the first column and first row
-    for i in range(len(a) + 1):
-        matrix[i][0] = i
-    for j in range(len(b) + 1):
-        matrix[0][j] = j
-
-    # Iterate through the matrix and update values
-    for i in range(1, len(a) + 1):
-        for j in range(1, len(b) + 1):
-            if a[i - 1] == b[j - 1]:
-                cost = 0
-            else:
-                cost = 1
-
-            matrix[i][j] = min(
-                matrix[i - 1][j] + 1,
-                matrix[i][j - 1] + 1,
-                matrix[i - 1][j - 1] + cost,
-            )
-
-    return matrix[-1][-1]
-
-
-def normalize_formula(formula):
-    return formula.replace(" ", "")
-
-
-def compare_formulas(formula1, formula2):
-    formula1_norm = normalize_formula(formula1)
-    formula2_norm = normalize_formula(formula2)
-    distance = levenshtein(formula1_norm, formula2_norm)
-    max_length = max(len(formula1_norm), len(formula2_norm))
-    if max_length == 0:
-        return 100.0
-    return (1 - distance / max_length) * 100
-
-
-print("Formulas from the first document:", math1)
-print("Formulas from the second document:", math2)
-
-for i, formula1 in enumerate(math1):
-    for j, formula2 in enumerate(math2):
-        similarity = compare_formulas(formula1, formula2)
-        print(
-            f"Similarity percentage between formula {i+1} from document 1 and formula {j+1} from document 2: {similarity:.2f}%"
-        )
+    # for i, formula1 in enumerate(math1):
+    #     for j, formula2 in enumerate(math2):
+    #         similarity = compare_formulas(formula1, formula2)
+    #         print(
+    #             f"Similarity percentage between formula {i+1} from document 1 and formula {j+1} from document 2: {similarity:.2f}%"
+    #         )
