9892fc919a1d36765ce65b246435f4383162713c
Jakub Pietrasik <jakubmkjp@gmail.com>
2024-01-08 15:52:34 +0100

    bug with threshold fixed proper message for lack of similarities added

--- backend/algorithms/levenshtein.py
+++ backend/algorithms/levenshtein.py
@@ -60,6 +60,7 @@ class L_FormulaComparer:
         for i, formula1 in enumerate(math1):
             for j, formula2 in enumerate(math2):
                 similarity = self.compare_formulas(formula1, formula2)
+                print(threshold)
                 if similarity > float(threshold):
                     result += f"Wykryto podobieństwo na poziomie {'%.2f'%(similarity)}% między formułą {i+1} z pierwszego dokumentu, a formułą {j+1} z drugiego dokumentu\n\n"
         return result
